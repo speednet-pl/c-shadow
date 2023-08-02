0fda751ddce2393c0d831a746df01c07725a9756
Mikołaj Kąkol <mikolaj.kakol@speednet.pl>
2023-08-02 20:40:40 +0200

    added publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("root.publication")
+    id("module.publication")
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
